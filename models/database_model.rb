507cb068cf1e9975dfda422b8517ab725d356822
Mac <mac@Macs-MacBook-Air.local>
2017-01-25 17:55:01 +0800

    testing

--- models/database_model.rb
+++ models/database_model.rb
@@ -4,9 +4,59 @@ module Database
   class InvalidAttributeError < StandardError;end
   class NotConnectedError < StandardError;end
 
-  class Model
+  class Model  
     def self.inherited(klass)
     end
+
+     def self.create(attributes)
+    record = self.new(attributes)
+    record.save
+
+    record
+  end
+
+  def self.where(query, *args)
+    Database::Model.execute("SELECT * FROM cohorts WHERE #{query}", *args).map do |row|
+      Cohort.new(row)
+    end
+  end
+
+  def self.find(pk)
+    self.where('id = ?', pk).first
+  end
+
+   def self.attribute_names
+    [:id, :name, :created_at, :updated_at]
+
+  attr_reader :attributes, :old_attributes
+  
+  end 
+
+  # e.g., Cohort.new(:id => 1, :name => 'Alpha', :created_at => '2012-12-01 05:54:30')
+  def initialize(attributes = {})
+    attributes.symbolize_keys!
+    raise_error_if_invalid_attribute!(attributes.keys)
+
+    @attributes = {}
+
+    Cohort.attribute_names.each do |name|
+      @attributes[name] = attributes[name]
+    end
+
+    @old_attributes = @attributes.dup
+  end
+
+    def [](attribute)
+    raise_error_if_invalid_attribute!(attribute)
+
+    @attributes[attribute]
+  end
+
+  def []=(attribute, value)
+    raise_error_if_invalid_attribute!(attribute)
+
+    @attributes[attribute] = value
+  end
 
     def self.connection
       @connection
@@ -32,6 +82,18 @@ module Database
     def self.attribute_names
       @attribute_names
     end
+
+
+  def save
+    if new_record?
+      results = insert!
+    else
+      results = update!
+    end
+      @old_attributes = @attributes.dup
+
+    results
+  end
 
     def self.attribute_names=(attribute_names)
       @attribute_names = attribute_names
@@ -77,6 +139,37 @@ module Database
     end
 
     private
+      private
+  def insert!
+    self[:created_at] = DateTime.now
+    self[:updated_at] = DateTime.now
+
+    fields = self.attributes.keys
+    values = self.attributes.values
+    marks  = Array.new(fields.length) { '?' }.join(',')
+
+    insert_sql = "INSERT INTO cohorts (#{fields.join(',')}) VALUES (#{marks})"
+
+    results = Database::Model.execute(insert_sql, *values)
+
+    # This fetches the new primary key and updates this instance
+    self[:id] = Database::Model.last_insert_row_id
+    results
+  end
+
+  def update!
+    self[:updated_at] = DateTime.now
+
+    fields = self.attributes.keys
+    values = self.attributes.values
+
+    update_clause = fields.map { |field| "#{field} = ?" }.join(',')
+    update_sql = "UPDATE cohorts SET #{update_clause} WHERE id = ?"
+
+    # We have to use the (potentially) old ID attributein case the user has re-set it.
+    Database::Model.execute(update_sql, *values, self.old_attributes[:id])
+  end
+
     def self.prepare_value(value)
       case value
       when Time, DateTime, Date
